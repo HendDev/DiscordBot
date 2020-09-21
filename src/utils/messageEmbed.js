d88d4ce8c124f207981a2936d23538936d241023
Salomón Sánchez <hend@iMac-de-Salomon.local>
2020-09-21 15:00:06 -0500

    final redes

--- src/utils/messageEmbed.js
+++ src/utils/messageEmbed.js
@@ -3,13 +3,9 @@ const Discord = require('discord.js');
 const messageEmbed = ({ authorName, title, content }) => {
   const parseContent = content.map(i => `${i}\n`)
   const msgEmbed = new Discord.MessageEmbed()
-    .setColor('#723ceb')
-    .setTitle(`Hola ${authorName}`)
-    .setImage(message.author.avatarURL)
-    .setThumbnail(message.author.avatarURL)
     .setColor('#0099ff')
+    .setTitle(`Hola ${authorName}`)
     .setDescription(`${title} ${parseContent}`)
-    .setColor('#723ceb')
     .setFooter('Comunidad Hend')
 
   return msgEmbed;
